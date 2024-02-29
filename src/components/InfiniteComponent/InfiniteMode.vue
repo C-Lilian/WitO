82d0e91a07377318a939dc49520bf2bcf2418df7
Lilian Cleret <lilian.cleret@gmail.com>
2024-02-29 19:14:07 +0100

    InfiniteMode V1

--- src/components/InfiniteComponent/InfiniteMode.vue
+++ src/components/InfiniteComponent/InfiniteMode.vue
@@ -1,3 +1,73 @@
+<script setup>
+
+var lstIconSimple = [
+  "dove", "dove",
+  "cow", "cow",
+  "hippo", "hippo",
+  "otter", "otter",
+  "dragon", "dragon",
+  "shrimp", "shrimp",
+  "locust", "locust",
+  "frog", "frog"
+]
+var lstIcons = [...lstIconSimple, ...lstIconSimple]
+var shuffleLstIcons = lstIcons.sort(() => (Math.random() > .5) ? 1 : -1);
+
+const emit = defineEmits(['goHome']);
+
+var displayMode = (mode) => {
+  emit('goHome',mode);
+};
+
+var lstIconsClickede = [];
+var cardClicked = 0;
+var pairDone = 0;
+
+function cardSelect(event) {
+  let card = event.currentTarget;
+  
+  if (card.classList[1] != 'validate') {
+    if (card.classList[1] != 'active') {
+      if (cardClicked < 2) {
+        card.classList.add('active');
+        if (card.childNodes[0].tagName === 'INPUT') {
+          lstIconsClickede.push(card.childNodes[0].value);
+        }
+        cardClicked++;
+      }
+      if (cardClicked == 2) {
+        var lstCardClicked = document.getElementsByClassName('active');
+        setTimeout(function() {
+            if (cardClicked != 2) {return;}
+            if (lstIconsClickede[0] == lstIconsClickede[1]) {
+              lstCardClicked[1].classList.add('validate')
+              lstCardClicked[1].classList.remove('active')
+              lstCardClicked[0].classList.add('validate')
+              lstCardClicked[0].classList.remove('active')
+              pairDone++;
+              if (pairDone == 8) {
+                alert("Bravo, vous avez gagné !")
+              }
+            } else {
+              lstCardClicked[1].classList.remove('active')
+              lstCardClicked[0].classList.remove('active')
+            }
+            lstIconsClickede = [];
+            lstCardClicked = [];
+            cardClicked = 0;
+        }, 1000);
+      }
+      
+    } else {
+      card.classList.remove('active');
+      cardClicked--;
+    }
+  }
+  
+}
+
+</script>
+
 <template>
   <div class="bodyDiv">
     <div class="infiniteDiv">
@@ -7,67 +77,22 @@
         <p>Reload</p>
       </div>
       <div class="bodyInfiniteDiv">
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
-        <div class="card card1">
-          <span class="cardBack">?</span>
-        </div>
+        <template v-for="n in 16" :key="n">
+          <div :id="`card-${n}`" class="card" @click="cardSelect($event)">
+            <input type="hidden" name="icon" :value=shuffleLstIcons[n-1]>
+            <div class="card-side back">
+              <span class="cardBack">?</span>
+            </div>
+            <div class="card-side front">
+              <span>
+                <font-awesome-icon :icon="['fas', shuffleLstIcons[n-1]]" />
+              </span>
+            </div>
+          </div>
+        </template>
       </div>
     </div>
   </div>
 </template>
-
-<script setup>
-
-const emit = defineEmits(['goHome']);
-
-const displayMode = (mode) => {
-  emit('goHome',mode);
-};
-
-</script>
 
 <style lang="scss">@import './InfiniteStyle.scss';</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* add icons to the library */
+library.add(fas)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
